07c1dc6d9daaa5724c81080c109aab38a6f8e7ad
Erdogan Bulut <e-bulut@hotmail.com.tr>
2021-10-19 19:13:13 +0300

    Configure mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FileModule } from './modules/file/file.module';
 
 @Module({
-  imports: [FileModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://dbUser:anG3e9Sf4JvEXDy@cluster0.kauei.mongodb.net/sample?retryWrites=true&w=majority',
+    ),
+    FileModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
